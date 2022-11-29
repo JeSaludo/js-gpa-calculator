1d03455c5650b205ac4e8fff9af39d44cd59cd2d
Jan Eris Saludo <janerissaludo@gmail.com>
2022-11-29 10:52:03 +0800

    Added Formula

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,25 @@
 //gpa formula =  -4 / number of items * score + 5
 // gpa => 1 - 3 text color green
 // gpa => 3 - 5 text color red
+
+let score = document.getElementById("score");
+let numOfItem = document.getElementById("noItems");
+let scoreGpa = document.getElementById("scoreGpa");
+let gpa = 0;
+
+document.getElementById("btnCalc").addEventListener('click', viewScore);
+function viewScore(){
+   
+   gpa = -4 / numOfItem.value * score.value + 5;
+
+    if(gpa >= 3 && gpa <= 5)
+        scoreGpa.style.color= "red";
+    else{
+        scoreGpa.style.color= "green";
+    }
+
+    scoreGpa.value = gpa;
+
+
+
+}
